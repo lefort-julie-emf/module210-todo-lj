93638350ca3e29b874ccb2b9e30f37aa85d807ca
lefort-julie-emf <julie.lefort@studentfr.ch>
2025-02-06 10:16:48 +0100

    ex3

--- src/frontend/app.js
+++ src/frontend/app.js
@@ -1,4 +1,4 @@
-const apiEndpoint = "http://localhost:8080/api/tasks";
+const apiEndpoint = "http://13.74.243.230:8080/api/tasks";
 
 $(document).ready(function () {
   // Charger les tâches au démarrage
