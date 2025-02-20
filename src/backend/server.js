e31beb46abeb4d8bea239a272838a5880d4419df
lefort-julie-emf <julie.lefort@studentfr.ch>
2025-02-20 08:22:17 +0100

    CEM

--- src/backend/server.js
+++ src/backend/server.js
@@ -13,7 +13,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 if (process.env.NODE_ENV === 'production') {
-	app.use(express.static('client/build'));      
+	app.use(express.static('client/build'));                                                                 
 }
 
 app.use("/api/tasks", tasksRoutes);
